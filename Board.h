ca89fa3128d91741a5ae23a86d0cd34e0408aa40
Chance Penner <chance.penner2016@gmail.com>
2019-09-14 16:30:00 -0500

    Add function definition comments that Markus deleted on accident

--- Board.h
+++ Board.h
@@ -8,19 +8,24 @@
 class Board
 {
 	private:
-		std::string myBoard[8][8];
-		
 		std::string shotBoard[8][8];
+		std::string myBoard[8][8];
 		std::string m_rowNames = "ABCDEFGH";
 		std::string m_columnNames = "12345678";
 		int rowIndex;
-		int ColumnIndex;
-		void guessConversion(std::string guess);
+		int columnIndex;
+		void guessConversion(std::string userGuess);	//converts userGuess to two array indices, updating rowIndex and columnIndex
+
 	public:
 		Board();
 		void printShotBoard();
 		void printMyBoard();
-		bool withinBoundary(std::string userGuess);
 
-};
+		void setupBoard(int numberOfShips); //asks the player where they want to place the ships. uses with withinBoundary and edits myBoard and shotBoard
+
+		void updateShotBoard(std::string userGuess, bool wasHit); //if blueTilde, change to whiteMiss, if whiteMiss, prints you already shot here, if redHit, prints you already shot here, if ship, change to redHit
+		void updateMyBoard(std::string userGuess); //if blueTilde, change to whiteMiss, if whiteMiss, prints you already shot here, if redHit, prints you already shot here, if ship, change to redHit
+
+		bool withinBoundary(std::string userGuess);	//gets called from Player shoot function. Calls guessConversion. Needs to check for valid input.
+	};
 #endif
