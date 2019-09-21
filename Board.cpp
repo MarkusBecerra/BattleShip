11d5b910d2293d134a6e03ca0e2f70482f51fac6
Chance Penner <chance.penner2016@gmail.com>
2019-09-21 17:44:30 -0500

    Fix merge issue which copied too much into guessConversion() function

--- Board.cpp
+++ Board.cpp
@@ -203,30 +203,21 @@ void Board::guessConversion(std::string userGuess) //converts userGuess to two i
 bool Board::withinBoundary(std::string userGuess) //a check for valid input still needs to be made, either here or where the user inputs the guess
 {
 	if(userGuess.length() != 2)
-	{
-		return false;
-	}
-	else
-	{
-	guessConversion(userGuess);
-	if((0 <= m_rowIndex && m_rowIndex <= 7) && (0 <= m_columnIndex && m_columnIndex <= 7))
-	{
-		return false;
-	}
-	else
-	{
-		guessConversion(userGuess);
-		if((0 <= m_rowIndex && m_rowIndex <= 7) && (0 <= m_columnIndex && m_columnIndex <= 7))
-		{
-			return true;
-		}
-		else
 		{
 			return false;
 		}
-	}
-
-	}
+		else
+		{
+			guessConversion(userGuess);
+			if((0 <= m_rowIndex && m_rowIndex <= 7) && (0 <= m_columnIndex && m_columnIndex <= 7))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
 }
 
 bool Board::noHorizontalCollision(std::string userGuess, int shipLength)
